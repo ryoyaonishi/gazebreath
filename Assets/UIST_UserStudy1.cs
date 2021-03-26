0e1e0d959ce63140c23737b52065125355b1b99a
onishiryoya <onishi@hapis.k.u-tokyo.ac.jp>
2021-03-26 14:53:26 +0900

    実験実装

--- Assets/UIST_UserStudy1.cs
+++ Assets/UIST_UserStudy1.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using System.IO.MemoryMappedFiles;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
-
-using System.IO;
-using System.Text;
-using System;
 
 public class UIST_UserStudy1 : MonoBehaviour
 {
@@ -29,12 +25,26 @@ public class UIST_UserStudy1 : MonoBehaviour
 
     public Vector3 objectPosition = new Vector3(0, 0, 0);
 
-   
+    private StreamWriter writer_for_exp1_breath;
+    private StreamWriter writer_for_exp1_dwell;
 
+    private bool isExp_Dwell;
+    private bool isExp_Breath;
+
+    private float exp1_startTime = 0.0f;
+
+    private float dwell_startTime = 0.0f;
+
+    
 
     // Start is called before the first frame update
     async void Start()
     {
+        writer_for_exp1_breath = new StreamWriter(@"SaveData_onishi_b.csv", true, Encoding.GetEncoding("Shift_JIS"));
+
+        writer_for_exp1_dwell = new StreamWriter(@"SaveData_onishi_d.csv", true, Encoding.GetEncoding("Shift_JIS"));
+
+
 
         //呼吸情報を記録しているところにアクセス
         var sharedMemory = MemoryMappedFile.OpenExisting("SharedMemory");
@@ -60,7 +70,7 @@ public class UIST_UserStudy1 : MonoBehaviour
                         isShortExhale = accessor.ReadBoolean(2);
                         accessor.Write(2, false);
                     }
-               
+
                     isLongInhale = accessor.ReadBoolean(3);
                     isLongExhale = accessor.ReadBoolean(4);
 
@@ -77,57 +87,83 @@ public class UIST_UserStudy1 : MonoBehaviour
         //このオブジェクトの位置を取得
         objectPosition = this.gameObject.transform.position;
 
-
         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //gazeCubeObj = GameObject.Find("GazeCube"); //マイフレーム取得する必要ある？
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(0,255,0,50);
 
+            isExp_Breath = true;
+            isExp_Dwell = false;
+            writer_for_exp1_breath.WriteLine("Start");
 
+            exp1_startTime = Time.time;
 
-        //if (!isInObject)
-        //{
-        //    return;
-        //}
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color32(0, 255, 0, 50);
+
+            isExp_Breath = false;
+            isExp_Dwell = true;
+            
+            writer_for_exp1_dwell.WriteLine("Start");
+
+            exp1_startTime = Time.time;
+        }
+
 
         if (isShortInhale)
         {
-            ClickAction();
+            //視線がオブジェクト内部にあるとき
+            if (isInObject)
+            {
+                ClickAction();
+                Debug.Log("isClicked");
+                isDragModeOn = true;
+            }
+      
             isShortInhale = false;
-            Debug.Log("isClicked");
 
-            isDragModeOn = true;
         }
         else if (isShortExhale)
         {
-            ClickAction();
-            isShortExhale = false;
-            Debug.Log("isClicked");
+            if (isInObject)
+            {
+                ClickAction();
+                Debug.Log("isClicked");
+                isDragModeOn = false;
+            }
 
-            isDragModeOn = false;
-        }
-        else
-        {
+            isShortExhale = false;
 
         }
 
         if (isLongInhale)
-        {
-            Debug.Log("Looooooooooooong Inhale");
-            DrawingAction();
+        {          
+            if (isInObject)
+            {
+                DrawingAction();
+            }
+
         }
         else if (isLongExhale)
         {
-            PushingAction();
+            if (isInObject)
+            {
+                PushingAction();
+            }
+
         }
 
         //ApplicationStudy();
-
-     
 
 
     }
@@ -135,30 +171,35 @@ public class UIST_UserStudy1 : MonoBehaviour
 
 
     private void ClickAction()
-    { 
-        //視線がオブジェクト内部にあるとき
-        if (isInObject)
+    {
+
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        if (Time.time > exp1_startTime)
         {
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            writer_for_exp1_breath.WriteLine(Time.time - exp1_startTime);
+            writer_for_exp1_breath.WriteLine("Finish");
         }
+
 
     }
 
     private void PushingAction()
     {
         //TODO: 奥に押し込む
-        if (isInObject) {
-            gameObject.transform.position += new Vector3(0,0,velocity * Time.deltaTime);
-        }
+
+        gameObject.transform.position += new Vector3(0, 0, velocity * Time.deltaTime);
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+
     }
 
     private void DrawingAction()
     {
         //TODO: 手前に引き寄せる
-        if (isInObject)
-        {
-            gameObject.transform.position -= new Vector3(0, 0, velocity * Time.deltaTime);
-        }
+
+        gameObject.transform.position -= new Vector3(0, 0, velocity * Time.deltaTime);
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
     }
 
     private void ApplicationStudy()
@@ -184,17 +225,40 @@ public class UIST_UserStudy1 : MonoBehaviour
 
     void OnTriggerEnter(Collider t)
     {
+        if (isExp_Dwell)
+        {
+            dwell_startTime = Time.time;
 
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         isInObject = true;
+
+        gameObject.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+
+        if (isExp_Dwell)
+        {
+            if (Time.time - dwell_startTime > 700)
+            {
+                ClickAction();
+            }
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
     {
         isInObject = false;
+
+        gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+        if (isExp_Dwell)
+        {
+            dwell_startTime = 0;
+        }
     }
 
 
@@ -202,7 +266,10 @@ public class UIST_UserStudy1 : MonoBehaviour
     Vector3 Sphere_scale_not_selected = new Vector3(0.15f, 0.15f, 0.15f);
     private bool isMoveModeOn;
     private bool isDragModeOn;
+    private bool isInTargetObject;
 
-
-
+    private void OnApplicationQuit()
+    {
+        writer_for_exp1_breath.Dispose();
+    }
 }
